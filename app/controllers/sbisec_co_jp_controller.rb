2ea90b796b783cc8fb4931fff92da51e7ea74605
FUNABARA Masao <masao@masoo.jp>
2022-01-03 05:34:11 +0000

    update sbisec.co.jp password.

--- app/controllers/sbisec_co_jp_controller.rb
+++ app/controllers/sbisec_co_jp_controller.rb
@@ -2,8 +2,12 @@ class SbisecCoJpController < ApplicationController
   def index
     seeds = Passwords::Generators::DEFAULT_UPPERCASE_ALPHABETS +
       Passwords::Generators::DEFAULT_LOWERCASE_ALPHABETS +
-      Passwords::Generators::DEFAULT_NUMBERS
-    @password = Passwords::Generators.create(size: 10, seeds: seeds)
+      Passwords::Generators::DEFAULT_NUMBERS +
+      Passwords::Generators::DEFAULT_SYMBOLS
+    begin
+      @password = Passwords::Generators.create(size: 20, seeds: seeds)
+      symbols_count = @password.split("").select{ Passwords::Generators::DEFAULT_SYMBOLS.join.include?(_1) }.count
+    end while (symbols_count < 2)
     @entropy = Passwords::Generators.entropy(@password)
     @score = Passwords::Generators.score(@password)
 
