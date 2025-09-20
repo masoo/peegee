d69f85b89fa4c85e2fb96d77493942140a299df6
FUNABARA Masao <masao@masoo.jp>
2025-09-20 11:48:55 +0000

    Refactor JSON response handling and move JSON rendering to a separate view file

--- app/controllers/smart_ex_jp_controller.rb
+++ app/controllers/smart_ex_jp_controller.rb
@@ -5,13 +5,11 @@ class SmartExJpController < ApplicationController
     @score = Passwords::Generators.score(@password)
 
     respond_to do |format|
+      format.json do
+        render layout: false, formats: [ :json ], content_type: "application/json"
+      end
       format.any do
         render layout: false, formats: [ :text ], content_type: "text/plain"
-      end
-      format.json do
-        render layout: false, json: {
-          password: @password, entropy: @entropy, score: @score
-        }
       end
     end
   end
